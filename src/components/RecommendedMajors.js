c002c216ec9320dcc32ef9b24a40a75613cd299f
PrajithSai <sai.prajith@gmail.com>
2021-05-27 16:36:15 -0500

    Added no reccommendations screen and score

--- src/components/RecommendedMajors.js
+++ src/components/RecommendedMajors.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Collapse } from 'antd';
+import { Collapse, Empty } from 'antd';
 import { sortBy } from 'lodash';
 
 import { getRecommendations } from '../utils/majorsRelation';
 
 const { Panel } = Collapse;
 
-const N_RECOMMENDATIONS = 6;
+const N_RECOMMENDATIONS = 5;
 
 export default function RecommendedMajors() {
-  const { courseEnjoyment } = useSelector((state) => ({
-    courseEnjoyment: state.majors.courseEnjoyment,
+  const { fullState } = useSelector((state) => ({
+    fullState: state.majors,
   }));
-  const recommendations = sortBy(getRecommendations(courseEnjoyment), ['score'])
+  const recommendations = sortBy(getRecommendations(fullState), ['score'])
+    .filter((rec) => rec.score > 0)
     .reverse()
     .slice(0, N_RECOMMENDATIONS);
-  console.log({ recommendations });
+  const noRecsFound = recommendations.length === 0;
   return (
     <>
-      <Collapse defaultActiveKey={['1']}>
-        {recommendations.map((recc, index) => (
-          <Panel header={recc.major} key={index}>
-            Insert more details related to {recc.major}.
-          </Panel>
-        ))}
-      </Collapse>
+      {noRecsFound ? (
+        <Empty description="No recommendations found! Please enter/update your course grades and interests in the previous screens to get recommendations!" />
+      ) : (
+        <Collapse defaultActiveKey={['1']}>
+          {recommendations.map((recc, index) => (
+            <Panel
+              // header={recc.major}
+              header={`${recc.major}, Score: ${recc.score}`}
+              key={index}
+            >
+              Insert more details related to {recc.major}. <br />
+            </Panel>
+          ))}
+        </Collapse>
+      )}
     </>
   );
 }
